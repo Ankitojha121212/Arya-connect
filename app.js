946651d7ede0dbf475bfa925397833d77d6776e7
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-24 18:47:45 +0530

    Working on Websockets

--- app.js
+++ app.js
@@ -7,10 +7,48 @@ const path = require("path");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
+main().then(()=>{
+                 console.log("Database connected Successfully !");
+      }).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/aryaconnect');
+}
+
+// for socket.io
+const http = require('http');
+// const server = http.createServer(app);
+// const {Server} = require("socket.io");
+// const io =new Server(server);
+
+// app.get("/chats",(req,res)=>{
+//     res.sendFile(__dirname + '/index.html')
+// })
+
+// io.on("connection",(Socket)=>{
+//     console.log("a user connected");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/",(req,res)=>{
     res.render("layouts/boilerplate");
 });
+
+
+
 
 
 app.listen(3000,()=>{
