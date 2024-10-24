628e92cedd9cb710e0aa8d1e2df04ddb4f3cbef3
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-24 15:26:11 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+
+app.get("/",(req,res)=>{
+    res.render("layouts/index");
+});
+
+
+app.listen(3000,()=>{
+    console.log("Server is Started at port : ",3000);
+})
